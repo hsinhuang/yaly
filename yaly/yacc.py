e5f674519595fc1b5fce0151e8446e353cdc8234
leon-huang <leon@njuopen.com>
2013-11-06 15:28:08 +0800

    refator rules : pass

--- yaly/yacc.py
+++ yaly/yacc.py
@@ -8,7 +8,9 @@ class TokenStream:
     def __init__(self, lexer):
         self.__cache__ = None
         self.__tokens__ = lexer.get_next_token()
-    def get_next_token(self):
+    def __iter__(self):
+        return self
+    def next(self):
         """
         return the next token(type: Token), None when no token remains
         """
@@ -16,10 +18,7 @@ class TokenStream:
             token = self.__cache__
             self.__cache__ = None
             return token
-        try:
-            return self.__tokens__.next()
-        except StopIteration:
-            return None
+        return self.__tokens__.next()
     def push_back(self, token):
         """
         push the token back to stream
@@ -30,6 +29,141 @@ class TokenStream:
             )
         self.__cache__ = token
 
+class Rule:
+    """
+    a single rule for a nonterminal, and maybe this nonterminal
+    has other rules
+    """
+    def __init__(self, rule_spec, func):
+        """
+        `rule_spec` is a string which specifies the rule
+
+        format: `lhs : t1 t2 t3 ...`
+        """
+        self.__func__ = func
+        rp_rule = rule_spec.split(':')
+        if len(rp_rule) != 2:
+            raise SyntaxError(
+                'Syntax rule `%s` not valid' % rule_spec
+            )
+        self.__lhs__, rhs = rp_rule[0].strip(), rp_rule[1].strip()
+        import re
+        self.__rhs__ = re.split(r'\s+', rhs)
+        if not all([s.isupper() or s.islower() \
+            for s in self.__rhs__] + [self.__lhs__.islower()]):
+            raise SyntaxError(
+                'terminals in rules should be uppercase, \
+                while nonterminals in rules should be lowercase'
+            )
+        self.__nonterminals__ = { self.__lhs__ }
+        self.__terminals__ = set()
+        for _id in self.__rhs__:
+            if _id.islower():
+                self.__nonterminals__.add(_id)
+            else:
+                self.__terminals__.add(_id)
+        self.__rule_spec__ = ' '.join([self.__lhs__, ':'] + self.__rhs__)
+    def __str__(self):
+        return self.__rule_spec__
+    def __eq__(self, other):
+        return self.__lhs__ == other.__lhs__ and \
+            self.__rhs__ == other.__rhs__
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    def __hash__(self):
+        return self.__rule_spec__.__hash__()
+    def terminals(self):
+        """getter : terminals in this rule"""
+        return self.__terminals__
+    def nonterminals(self):
+        """getter : nonterminals in this rule"""
+        return self.__nonterminals__
+    def lhs(self):
+        """getter : lhs nonterminal in this rule"""
+        return self.__lhs__
+    def rhs(self):
+        """getter : list of rhs identifiers in this rule"""
+        return self.__rhs__
+
+class CompleteRule:
+    """a grammar rule for a nonterminal"""
+    def __init__(self, lhs):
+        self.__lhs__ = lhs
+        self.__rules__ = set()
+        self.__terminals__ = set()
+        self.__nonterminals__ = { self.__lhs__ }
+    def __iter__(self):
+        return self.__rules__
+    def __eq__(self, other):
+        return self.__lhs__ == other.__lhs__ and \
+            self.__rules__ == other.__rules__
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    def __str__(self):
+        return ' | '.join([rule.__str__() for rule in self.__rules__])
+    def add(self, rule):
+        """add a rule"""
+        if rule.lhs() != self.__lhs__:
+            raise TypeError(
+                'add a Rule of `%s` to a CompleteRule of `%s`' %\
+                (rule.lhs(), self.__lhs__)
+            )
+        self.__rules__.add(rule)
+        self.__terminals__.update(rule.terminals())
+        self.__nonterminals__.update(rule.nonterminals())
+        return self
+    def terminals(self):
+        """getter : terminals in all rules"""
+        return self.__terminals__
+    def nonterminals(self):
+        """getter : nonterminals in all rules"""
+        return self.__nonterminals__
+    def lhs(self):
+        """getter : lhs nonterminal in all rules"""
+        return self.__lhs__
+    def rules(self):
+        """getter : all rules"""
+        return self.__rules__
+
+class Rules:
+    """a container of all CompleteRule's"""
+    def __init__(self):
+        self.__rules__ = {}
+        self.__terminals__ = set()
+        self.__nonterminals__ = set()
+    def __getitem__(self, lhs):
+        self.__terminals__ = None
+        self.__nonterminals__ = None
+        return self.__rules__[lhs]
+    def __setitem__(self, lhs, complete_rule):
+        if not complete_rule:
+            complete_rule = CompleteRule(lhs)
+        self.__rules__[lhs] = complete_rule
+        self.__terminals__.update(complete_rule.terminals())
+        self.__nonterminals__.update(complete_rule.nonterminals())
+        return self
+    def __delitem__(self, key):
+        del self.__rules__[key]
+    def __len__(self):
+        return len(self.__rules__)
+    def __iter__(self):
+        return self.__rules__.__iter__()
+    def __str__(self):
+        return '\n'.join([self[lhs].__str__() for lhs in self])
+    def terminals(self):
+        """getter : terminals in all rules"""
+        if not self.__terminals__:
+            self.__terminals__ = reduce(lambda x, y : x.union(y),
+                [self[lhs].terminals() for lhs in self],
+                set())
+        return self.__terminals__
+    def nonterminals(self):
+        """getter : nonterminals in all rules"""
+        if not self.__nonterminals__:
+            self.__nonterminals__ = reduce(lambda x, y : x.union(y),
+                [self[lhs].nonterminals() for lhs in self],
+                set())
+        return self.__nonterminals__
 
 class LL1Parser:
     """a defined LL(1) CFG Parser"""
