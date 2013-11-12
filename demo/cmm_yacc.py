b0a70bf250002dbca8aa196350bdf9c698a86e2f
leon-huang <leon@njuopen.com>
2013-11-12 11:42:07 +0800

    use full path of file

--- demo/cmm_yacc.py
+++ demo/cmm_yacc.py
@@ -3,14 +3,16 @@
 
 """yacc definition for C--"""
 
+import os.path as p
+
 import yaly.yacc as yacc
 from cmm_lex import lexer, tokens
 
-with open('cmm.bnf.txt', 'r') as f:
+with open(p.join(p.dirname(__file__), 'cmm.bnf.txt'), 'r') as f:
     grammar = [ line.strip() for line in f.xreadlines() if line.strip() ]
 
 parser = yacc.yacc()
 
-with open('cmm_input.c', 'r') as f:
+with open(p.join(p.dirname(__file__), 'cmm_input.c'), 'r') as f:
     s = f.read()
 parser.parse(s)
