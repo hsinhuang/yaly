7fe58504b6f71fc6f1f46402c062d4195929defc
leon-huang <leon@njuopen.com>
2013-11-13 09:50:15 +0800

    update pkg info

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ except ImportError:
 setup(name = "yaly",
     description="Yet Another Lex-Yacc",
     long_description = """
-YALY is yet another simple implementation lex and Lex-Yacc
-implemented in Python.
-It is fit for teaching and learning.
+YALY is yet another simple implementation of Lex-Yacc
+which is implemented in Python.
+It can be used for teaching and learning.
 It is compatible with both Python 2 and Python 3.
 """,
     license="""GPL v2""",
@@ -17,10 +17,10 @@ It is compatible with both Python 2 and Python 3.
     author_email = "leon@njuopen",
     maintainer = "Leonardo Huang",
     maintainer_email = "leon@njuopen",
-    url = "https://github.com/leon-huang/yalexer",
+    url = "https://github.com/leon-huang/yaly",
     packages = ['yaly'],
     install_requires = ['yare'],
-    dependency_links = ['https://github.com/leon-huang/yare/tarball/master#egg=yare-0.4'],
+    dependency_links = ['https://github.com/leon-huang/yare/tarball/master#egg=yare'],
     classifiers = [
       'Programming Language :: Python :: 3',
       'Programming Language :: Python :: 2',
